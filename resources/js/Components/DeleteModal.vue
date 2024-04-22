f1b506690dddb564c294276c98fbad4b5989eb63
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-04-23 02:03:02 +0300

    Modify delete modal displays

--- resources/js/Components/DeleteModal.vue
+++ resources/js/Components/DeleteModal.vue
@@ -1,12 +1,19 @@
 <template>
   <div v-if="show" id="delete-modal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
     <div class="bg-white rounded-lg shadow-lg p-6">
-      <h3 class="text-xl font-semibold text-gray-900">Είστε σίγουροι ότι θέλετε να διαγράψετε το ακίνητο?</h3>
-      <h5 v-if="!deleted" class=" mt-5 mb-5 text-l text-center text-gray-900">Κωδικός ακινήτου: #{{code}}</h5>
-      <h5 v-else class=" mt-5 mb-5 text-l text-center text-gray-900">Το ακίνητο διαγράφηκε επιτυχώς</h5>
+      <div v-if="!deleted">
+        <h3 class="text-xl font-semibold text-gray-900">Είστε σίγουροι ότι θέλετε να διαγράψετε το ακίνητο?</h3>
+        <h5 class=" mt-5 mb-5 text-l text-center text-gray-900">Κωδικός ακινήτου: #{{ code }}</h5>
+      </div>
+      <div v-else>
+        <h3 class="text-xl font-semibold text-center text-gray-900">Το ακίνητο διαγράφηκε επιτυχώς</h3>
+      </div>
       <div class="mt-4 flex justify-between space-x-4">
-        <button v-if="!deleted" @click="deleteProperty" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg px-4 py-2" type="button">Διαγραφή</button>
-        <button @click="closeModal" class="text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg px-4 py-2" type="button">Ακύρωση</button>
+        <button v-if="!deleted" @click="deleteProperty" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg px-4 py-2"
+                type="button">Διαγραφή
+        </button>
+        <button @click="closeModal" class="text-gray-700 bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg px-4 py-2" type="button">Ακύρωση
+        </button>
       </div>
     </div>
   </div>
@@ -27,10 +34,11 @@ const closeModal = () => {
 const deleteProperty = () => {
     emit('delete');
     deleted.value = true;
+    console.log(show.value)
 };
 defineProps({
     code: {
-        type: Number,
+        type: String,
     }
 });
 </script>
