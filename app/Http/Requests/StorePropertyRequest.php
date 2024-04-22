f09da863b34a15fd4b094b58092cec79dc558f77
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-04-23 01:07:39 +0300

    Add request class for property validation

---
+++ app/Http/Requests/StorePropertyRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePropertyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'price' => 'required|numeric|between:50,5000000',
+            'location' => 'required|numeric',
+            'availability_type' => 'required|numeric',
+            'area_sq' => 'required|numeric|between:20,1000',
+        ];
+    }
+}
